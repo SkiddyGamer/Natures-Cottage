c9b8d772218fcc756b30bc41e6f91eca70e7cca7
EgyptPhantom <114050577+EgyptPhantom@users.noreply.github.com>
2025-02-13 21:15:33 +0200

    Update Header.php

--- Header.php
+++ Header.php
@@ -1,16 +1,25 @@
 <header>
-        <div class="logo">Nature's<span> Cottage</span></div>
-        <nav>
-            <ul class="nav-links">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="listings.php">Browse properties</a></li>
-                <li><a href="details.php">List your property</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-            <div class="auth-buttons">
+    <div class="logo">Nature's<span> Cottage</span></div>
+    <nav>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="listings.php">Browse properties</a></li>
+            <li><a href="details.php">List your property</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+
+        <div class="auth-buttons">
+            <?php if (isset($_SESSION['user_id'])): ?>
+                
+                <a href="account.php"><i class="fas fa-user"></i></a>
+
+
+            <?php else: ?>
+                
                 <button class="btn" id="signInButton">Sign In</button>
                 <button class="btn primary" id="signUpButton">Sign Up</button>
-            </div>
-        </nav>
-    </header>
+            <?php endif; ?>
+        </div>
+    </nav>
+</header>
