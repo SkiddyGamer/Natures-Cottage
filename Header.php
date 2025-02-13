32b92497a71ca4ab38870141ed724a593e115f24
EgyptPhantom <114050577+EgyptPhantom@users.noreply.github.com>
2025-02-13 11:13:34 +0200

    Create Header.php

---
+++ Header.php
@@ -0,0 +1,16 @@
+<header>
+        <div class="logo">Nature's<span> Cottage</span></div>
+        <nav>
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="listings.php">Browse properties</a></li>
+                <li><a href="details.php">List your property</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <div class="auth-buttons">
+                <button class="btn" id="signInButton">Sign In</button>
+                <button class="btn primary" id="signUpButton">Sign Up</button>
+            </div>
+        </nav>
+    </header>
